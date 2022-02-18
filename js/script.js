532027777f2c75cb3d8afbf5a8084ad17126efcf
Simon <s.dunvall@gmail.com>
2022-02-18 22:01:50 +0100

    mail validation

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-function showdev1(){
+function showdev1() {
     $("#content").load("dev1.html");
 }
 
-function showdev2(){
+function showdev2() {
     $("#content").load("dev2.html");
 }
 
-function showdev3(){
+function showdev3() {
     $("#content").load("dev3.html");
+}
+
+function emailvalidation(inputText) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if (inputText.match(mailformat)) {
+        document.getElementById("email").style.color = "green";
+    }
+    else 
+    {
+        document.getElementById("email").style.color = "red";
+    }
+
 }
